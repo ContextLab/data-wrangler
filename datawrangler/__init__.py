1678abbdb2d530f5f1003bf039ac93c163c3c7d3
jeremymanning <jeremy.manning@gmail.com>
2021-07-10 22:31:33 -0400

    updating __init__.py files [WIP]

--- datawrangler/__init__.py
+++ datawrangler/__init__.py
@@ -4,7 +4,10 @@ __author__ = """Contextual Dynamics Lab"""
 __email__ = 'contextualdynamics@gmail.com'
 __version__ = '0.1.0'
 
-import format  # FIXME: this doesn't work...
-import helpers
-import io
-import decorate
+from .format import wrangle
+from .helpers import btwn, dataframe_like, array_like, load_dataframe
+from .io import load, save
+from .decorate import list_generalizer, funnel, fill_missing, interpolate, stack_handler, module_checker, \
+    unstack_apply, stack_apply, apply_defaults
+
+# TODO: organize imports into modules (need to make folders with their own __init__.py files)
