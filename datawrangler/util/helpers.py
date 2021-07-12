64b1d43eb26442e52ac612ee4c8eef21d228d7ee
jeremymanning <jeremy.manning@gmail.com>
2021-07-11 22:02:58 -0400

    tests are running OK now.  a bunch are failing but...progress!

--- datawrangler/util/helpers.py
+++ datawrangler/util/helpers.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import modin
 import six
+import warnings
 
 from ..io.io import get_extension
 from ..format.array import is_array
--- tests/wrangler/test_formats.py
+++ tests/wrangler/test_formats.py
@@ -7,9 +7,10 @@ import numpy as np
 import pandas as pd
 import os
 
-data_file = os.path.join('..', 'resources', 'testdata.csv')
-img_file = os.path.join('..', 'resources', 'wrangler.jpg')
-text_file = os.path.join('..', 'resources', 'home_on_the_range.txt')
+resources = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'resources')
+data_file = os.path.join(resources, 'testdata.csv')
+img_file = os.path.join(resources, 'wrangler.jpg')
+text_file = os.path.join(resources, 'home_on_the_range.txt')
 
 data = pd.read_csv(data_file, index_col=0)
 
