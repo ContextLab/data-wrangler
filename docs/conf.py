bd0f97d7f711e62ec925d90ec2d3304e2a85bb8d
jeremymanning <jeremy.manning@gmail.com>
2021-07-17 23:47:08 -0400

    switch to pydata sphinx theme for a cleaner look

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,6 @@
 #
 import os
 import sys
-import sphinx_readable_theme
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -85,8 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
-html_theme = 'readable'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
