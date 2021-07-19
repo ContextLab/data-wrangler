8f4d3ae8be23e7a55b9e00c05e605b9e1796145f
jeremymanning <jeremy.manning@gmail.com>
2021-07-19 02:21:09 -0400

    speed up test, update pandas requirement for github actions

--- tests/wrangler/__init__.py
+++ tests/wrangler/__init__.py
@@ -1,8 +1,10 @@
-import datawrangler as dw
-import os
-from glob import glob as lsdir
+# comment out to run tests MUCH faster...
 
-# reset data directory
-data_directory = eval(dw.core.get_default_options()['data']['datadir'])
-for f in lsdir(os.path.join(data_directory, '*.*')):
-    os.remove(f)
+# import datawrangler as dw
+# import os
+# from glob import glob as lsdir
+#
+# # reset data directory
+# data_directory = eval(dw.core.get_default_options()['data']['datadir'])
+# for f in lsdir(os.path.join(data_directory, '*.*')):
+#     os.remove(f)
